78aab691a716a6952823ed3f66a33df3ed48a006
starsdreamever <13963437@qq.com>
2025-06-27 19:08:37 +0800

    add ip save define

--- components/at/src/at_web_server_cmd.c
+++ components/at/src/at_web_server_cmd.c
@@ -31,6 +31,7 @@
 #include "esp_flash_partitions.h"
 #include "esp_partition.h"
 #include "esp_mac.h"
+#include "esp_netif.h"
 
 #include "esp_at.h"
 
@@ -993,12 +994,12 @@ static esp_err_t at_web_apply_wifi_connect_info(int32_t udp_port)
 
     // Clear connect status flag
     at_web_update_sta_got_ip_flag(false);
-/*    esp_netif_dhcpc_stop(sta_if);
-  
+    esp_netif_dhcpc_stop(sta_if);
+    
   
     esp_netif_t *sta_if = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF");
     esp_netif_ip_info_t info_t;
-    
+
     memset(&info_t, 0, sizeof(esp_netif_ip_info_t));
     info_t.ip.addr = esp_ip4addr_aton((const char *)connect_config.ip);
     info_t.netmask.addr = esp_ip4addr_aton((const char *)connect_config.nm);
@@ -1006,7 +1007,7 @@ static esp_err_t at_web_apply_wifi_connect_info(int32_t udp_port)
     ESP_LOGI(TAG, "static ip 1");
     ESP_ERROR_CHECK( esp_netif_set_ip_info(sta_if, &info_t));
     ESP_LOGI(TAG, "static ip 2");
-*/
+
     // According to config wifi device to try connect
     // when udp_port == -1, it's web browser post data to config wifi. otherwise, Now, It's WeChat post data to config wifi.
     // when (strlen((char *)connect_config->ssid) == 0) && (udp_port != -1), it's WeChat post data, and target AP is local phone.
