dee90ffbd312e8dbec90e01069dd09e96410db58
starsdreamever <13963437@qq.com>
2025-06-28 20:04:54 +0800

    ip save

--- components/at/src/at_web_server_cmd.c
+++ components/at/src/at_web_server_cmd.c
@@ -1200,7 +1200,7 @@ static esp_err_t at_get_wifi_info_from_json_str(char *buffer, wifi_sta_connect_c
             strncpy(ip, item->valuestring, ip_len);
         }
     }
-
+/*
     item = cJSON_GetObjectItem(root, "webnm");
     if (item) {
         nm_len = strlen(item->valuestring);
@@ -1224,7 +1224,7 @@ static esp_err_t at_get_wifi_info_from_json_str(char *buffer, wifi_sta_connect_c
             strncpy(gw, item->valuestring, gw_len);
         }
     }
-
+*/
     cJSON_Delete(root);
 
     memcpy(config->ssid, ssid, ssid_len);
