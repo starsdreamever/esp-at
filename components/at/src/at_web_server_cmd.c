43f304696083b431c233d32a082494b30e1a5c5d
starsdreamever <13963437@qq.com>
2025-11-27 07:52:33 +0800

    add rssi 3

--- components/at/src/at_web_server_cmd.c
+++ components/at/src/at_web_server_cmd.c
@@ -1553,13 +1553,9 @@ static esp_err_t ap_record_get_handler(httpd_req_t *req)
                 }
                 json_len += sprintf(temp_json_str + json_len, "%c", c);
             }
-            int32_t rssi_len = strlen((const char*)ap_info[loop].rssi);
-            for (int i = 0; i < rssi_len; i++) {
-                uint8_t c = ap_info[loop].rssi[i];
-                // escape special non-control characters in json format, see https://www.json.org/json-en.html for more details
-               
-                json_len += sprintf(temp_json_str + json_len, "%c", c);
-            }
+
+            json_len += sprintf(temp_json_str + json_len, "%c", ap_info[loop].rssi);
+            
 
             json_len += sprintf(temp_json_str + json_len, "\",\"auth_mode\":%d},", ap_info[loop].authmode);
 
